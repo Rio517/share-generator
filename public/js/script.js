145eb8e6dc225f1de53a96f26d9fac375b66f555
rio <rio517@gmail.com>
2011-11-19 19:13:57 -0500

    seperating out email_link and web_link

--- public/js/script.js
+++ public/js/script.js
@@ -3,19 +3,22 @@ lots of borrowed js from Jim Pugh
 
 */
 
+function displayExample(val){
+  var samples = { web_link: '<a href="#">Example website link</a>',
+                  email_link: '<a href="#">Example email link</a>',
+                  button: '<img src="http://sphotos.ak.fbcdn.net/hphotos-ak-ash2/hs329.ash2/60892_444262393552_19292868552_5172535_910697_n.jpg" />',
+                  icon_link: '<img src="http://sphotos.ak.fbcdn.net/hphotos-ak-snc4/hs606.snc4/58624_444262413552_19292868552_5172537_7739429_n.jpg" />',
+                  icon: '<img src="http://sphotos.ak.fbcdn.net/hphotos-ak-ash2/hs329.ash2/60927_444262408552_19292868552_5172536_687291_n.jpg" />'}
+  $('#sample_link').html(samples[val]);
+}
+
 $(document).ready(function(){
-      $('#fb_type').change(function(){
-        var samples = {button: '<img src="http://sphotos.ak.fbcdn.net/hphotos-ak-ash2/hs329.ash2/60892_444262393552_19292868552_5172535_910697_n.jpg" />',
-                      icon_link: '<img src="http://sphotos.ak.fbcdn.net/hphotos-ak-snc4/hs606.snc4/58624_444262413552_19292868552_5172537_7739429_n.jpg" />',
-                      icon: '<img src="http://sphotos.ak.fbcdn.net/hphotos-ak-ash2/hs329.ash2/60927_444262408552_19292868552_5172536_687291_n.jpg" />'}
-        var target = $('#sample_link');
-        if($(this).val()==''){
-          target.html('<a href="#">Example Text</a>');
-        }else{
-          target.html(samples[$(this).val()]);
-        }
-      });
-    });
+  displayExample('web_link');
+  $('#fb_type').change(function(){
+    displayExample($(this).val());
+  });
+});
+
   function shareFB() {
     var title = $('#fb_title').val();
     title = title.replace(/"/g,'\\"');
@@ -34,12 +37,18 @@ $(document).ready(function(){
         meta += '<meta property="og:site_name" content="'+site_name+'">';
         if(content_type != ''){ meta += '<meta property="og:type" content="'+content_type+'">\n' };
     var encoded_url = encodeURIComponent(url);
-    if (type==''){
-      var link = '<' + 'script>\nfunction fbs_click()\n{u="' + url + '";t="' + title + '";window.open(\'http://www.facebook.com/sharer.php?u=\'+encodeURIComponent(u)+\'&t=\'+encodeURIComponent(t),\'sharer\',\'toolbar=0,status=0,width=626,height=436\');return false;}\n</' + 'script>';
-      link += '\n<a class="fb_link" onclick="return fbs_click()" href="#">INSERT LINK CONTENTS HERE</a>';
-    }else{
-      var link = '<a name="fb_share" type="'+type+'" share_url="'+encoded_url+'">Link Text</a>';
-      link += '\n\n<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>';
+    
+    switch (type){
+      case 'web_link':
+        var link = '<' + 'script>\nfunction fbs_click()\n{u="' + url + '";t="' + title + '";window.open(\'http://www.facebook.com/sharer.php?u=\'+encodeURIComponent(u)+\'&t=\'+encodeURIComponent(t),\'sharer\',\'toolbar=0,status=0,width=626,height=436\');return false;}\n</' + 'script>';
+        link += '\n<a class="fb_link" onclick="return fbs_click()" href="#">INSERT LINK CONTENTS HERE</a>';
+        break;
+      case 'email_link':
+        var link = 'http://www.facebook.com/sharer.php?u='+encoded_url+'&t='+encodeURIComponent(title);
+        break;
+      default:
+        var link = '<a name="fb_share" type="'+type+'" share_url="'+encoded_url+'">Link Text</a>';
+        link += '\n\n<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>';
     }
     
     $('#fb_metatags').val(meta);
